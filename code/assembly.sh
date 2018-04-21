d4f8033780fb5f2e2316a8ce20cd49f7c74878a1
Unknown <toivoaon@hotmail.com>
2018-04-21 12:18:34 +0200

    Amended the sbatch script for PacBio assembly
    Changed the time from 6h->8h
    Changed from * to exact names of each file.

--- code/assembly.sh
+++ code/assembly.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2018003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 6:00:00
+#SBATCH -t 8:00:00
 #SBATCH -J PacBio_assembly
 #SBATCH --mail-type=ALL
 #SBATCH --lauri.mesilaakso.5423@student.uu.se
@@ -15,5 +15,10 @@ canu \
 -p efaecium 
 -d ../efaecium-pacbio \
 genomeSize=3m \
--pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_*.fastq.gz
+-pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_1.fastq.gz
+-pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_2.fastq.gz
+-pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_3.fastq.gz
+-pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_4.fastq.gz
+-pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_5.fastq.gz
+-pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_6.fastq.gz
 
