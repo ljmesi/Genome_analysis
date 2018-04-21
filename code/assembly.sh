8f770f2cc30c9ff854eeee719b2b981cd5be128a
Unknown <toivoaon@hotmail.com>
2018-04-21 13:35:28 +0200

    Amended the pacbio script so that maybe now it'll run
    Changed the file locations to their actual locations and not the symbolic links.

--- code/assembly.sh
+++ code/assembly.sh
@@ -15,10 +15,9 @@ canu \
 -p efaecium 
 -d ../efaecium-pacbio \
 genomeSize=3m \
--pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_1.fastq.gz
--pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_2.fastq.gz
--pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_3.fastq.gz
--pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_4.fastq.gz
--pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_5.fastq.gz
--pacbio-raw /home/lame5423/Genome_analysis/data/DNA/PacBio/PacBio_subread_6.fastq.gz
-
+-pacbio-raw /proj/g2018003/nobackup/private/zhang_2017/DNA_raw_data/PacBio/m131023_233432_42174_c100519312550000001823081209281335_s1_X0.1.subreads.fastq.gz
+-pacbio-raw /proj/g2018003/nobackup/private/zhang_2017/DNA_raw_data/PacBio/m131023_233432_42174_c100519312550000001823081209281335_s1_X0.2.subreads.fastq.gz
+-pacbio-raw /proj/g2018003/nobackup/private/zhang_2017/DNA_raw_data/PacBio/m131023_233432_42174_c100519312550000001823081209281335_s1_X0.3.subreads.fastq.gz
+-pacbio-raw /proj/g2018003/nobackup/private/zhang_2017/DNA_raw_data/PacBio/m131024_200535_42174_c100563672550000001823084212221342_s1_p0.1.subreads.fastq.gz
+-pacbio-raw /proj/g2018003/nobackup/private/zhang_2017/DNA_raw_data/PacBio/m131024_200535_42174_c100563672550000001823084212221342_s1_p0.2.subreads.fastq.gz
+-pacbio-raw /proj/g2018003/nobackup/private/zhang_2017/DNA_raw_data/PacBio/m131024_200535_42174_c100563672550000001823084212221342_s1_p0.3.subreads.fastq.gz
