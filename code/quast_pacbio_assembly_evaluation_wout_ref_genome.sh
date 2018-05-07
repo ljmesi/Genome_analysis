98f3758e686b8bfffd38b874f725eb22e2de51b0
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-07 19:48:54 +0200

    sbatch script in which trying to run quast without ref_genome

---
+++ code/quast_pacbio_assembly_evaluation_wout_ref_genome.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 01:00:00
+#SBATCH -J quast_pacbio_assembly_evaluation
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user lauri.mesilaakso.5423@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load quast/4.5.4
+
+# Your commands
+quast.py -o /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly_evaluation/Quast_1/ \
+/home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta
