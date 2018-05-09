73b588281b5b445d6a609765a43ed8b72480e685
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-09 11:06:30 +0200

    Adjusted parameters in trimmomatic script and added a new folder for output for illumina_nanopore evaluation

--- code/quast_illumina_nanopore_assembly_evaluation.sh
+++ code/quast_illumina_nanopore_assembly_evaluation.sh
@@ -12,6 +12,6 @@ module load bioinfo-tools
 module load quast/4.5.4
 
 # Your commands
-quast.py -o /home/lame5423/Genome_analysis/analyses/03_genome_assembly/Illumina_nanopore_assembly_evaluation/ \
+quast.py -o /home/lame5423/Genome_analysis/analyses/03_genome_assembly/Illumina_nanopore_assembly_evaluation/Quast/ \
 /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
--R /home/lame5423/Genome_analysis/data/DNA/RefGenome/efaecium_ref_genome.fasta \
+-R /home/lame5423/Genome_analysis/data/DNA/RefGenome/efaecium_ref_genome.fasta
