dd422876c50ff68dc4bc9a65d2d40a3f54e9667d
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-14 16:26:35 +0200

    Added sorted bam-file

--- code/pilon_corrected_pb_il_assembly.sh
+++ code/pilon_corrected_pb_il_assembly.sh
@@ -12,5 +12,5 @@ module load bioinfo-tools
 module load Pilon/1.22
 
 java -Xmx16G -jar /sw/apps/bioinfo/Pilon/1.22/rackham/pilon-1.22.jar \
---genome /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta --frags \
-/home/lame5423/Genome_analysis/code/aln-pb.bam
+--genome /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
+--frags /home/lame5423/Genome_analysis/code/aln-pb.sorted.bam
