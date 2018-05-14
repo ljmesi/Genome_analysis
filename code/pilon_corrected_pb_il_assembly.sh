86f7956f13f9487eae4a5852967376be16a44717
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-14 16:06:05 +0200

    Small error in the file name

--- code/pilon_corrected_pb_il_assembly.sh
+++ code/pilon_corrected_pb_il_assembly.sh
@@ -11,6 +11,6 @@
 module load bioinfo-tools
 module load Pilon/1.22
 
-java -Xmx16G -jar /sw/apps/bioinfo/Pilon/1.22/rackham/pilon \
+java -Xmx16G -jar /sw/apps/bioinfo/Pilon/1.22/rackham/pilon-1.22.jar \
 --genome /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta --frags \
 /home/lame5423/Genome_analysis/code/aln-pb.bam
