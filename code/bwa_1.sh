41db3c94d9516fd52336c44b16a54f3abf6a8eaf
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-14 10:04:13 +0200

    Removed invalid option -v

--- code/bwa_1.sh
+++ code/bwa_1.sh
@@ -12,7 +12,7 @@ module load bioinfo-tools
 module load bwa/0.7.17
 
 bwa index /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
-bwa mem -v 1 -M /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
+bwa mem -M /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
 /home/lame5423/Genome_analysis/data/DNA/Illumina/WGS_forward.fq.gz \
 /home/lame5423/Genome_analysis/data/DNA/Illumina/WGS_reverse.fq.gz \
 > aln-pe.sam
