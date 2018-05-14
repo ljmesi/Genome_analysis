7a064232106b34fab40220c6d064c1ff76c00862
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-14 08:48:54 +0200

    Corrected the syntax of bwa mem command

--- code/bwa.sh
+++ code/bwa.sh
@@ -5,11 +5,12 @@
 #SBATCH -t 3:00:00
 #SBATCH -J bwa
 #SBATCH --mail-type=ALL
-#SBATCH --lauri.mesilaakso.5423@student.uu.se
+#SBATCH --mail-user lauri.mesilaakso.5423@student.uu.se
 
 # Load modules
 module load bioinfo-tools
 module load bwa/0.7.17
 
 bwa index /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
-bwa mem  /home/lame5423/Genome_analysis/analyses/03_genome_assembly/Illumina_nanopore_assembly/contigs.fasta
+bwa mem /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
+/home/lame5423/Genome_analysis/analyses/03_genome_assembly/Illumina_nanopore_assembly/contigs.fasta
