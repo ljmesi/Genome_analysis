8c59fd2413a2c54df7b1130c9865e356387e637a
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-14 10:10:27 +0200

    Removed option -M

--- code/bwa_1.sh
+++ code/bwa_1.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2018003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 3:00:00
+#SBATCH -t 4:00:00
 #SBATCH -J bwa
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user lauri.mesilaakso.5423@student.uu.se
@@ -12,7 +12,7 @@ module load bioinfo-tools
 module load bwa/0.7.17
 
 bwa index /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
-bwa mem -M /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
+bwa mem /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
 /home/lame5423/Genome_analysis/data/DNA/Illumina/WGS_forward.fq.gz \
 /home/lame5423/Genome_analysis/data/DNA/Illumina/WGS_reverse.fq.gz \
 > aln-pe.sam
