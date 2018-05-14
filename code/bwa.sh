8ed99583aef49e820f0a8616617be4a2d8c35ad1
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-14 08:34:24 +0200

    Bwa indexing and mapping of the PacBio assembly with Illumina_Nanopore assembly contigs

---
+++ code/bwa.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 3:00:00
+#SBATCH -J bwa
+#SBATCH --mail-type=ALL
+#SBATCH --lauri.mesilaakso.5423@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load bwa/0.7.17
+
+bwa index /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
+bwa mem  /home/lame5423/Genome_analysis/analyses/03_genome_assembly/Illumina_nanopore_assembly/contigs.fasta
