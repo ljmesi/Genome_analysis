55f72ec07f57afd6afbf3e0762e72f0cb497b922
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-16 14:01:43 +0200

    Sbatch for Quast for pilon assembly

---
+++ code/Quast_PacBio_corrected_Illumina.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 01:00:00
+#SBATCH -J quast_PacBio_corrected_Illumina_evaluation
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user lauri.mesilaakso.5423@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load quast/4.5.4
+
+# Your commands
+quast.py /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_corrected_Illumina/pilon.fasta \
+-o /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_corrected_Illumina_evaluation/Quast \
+-R /home/lame5423/Genome_analysis/data/DNA/RefGenome/efaecium_ref_genome.fasta
