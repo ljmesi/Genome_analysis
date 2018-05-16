b0affca678e367c7550f60b0908a0f94c640a90f
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-16 14:57:07 +0200

    Added assembly before correction into the analysis and renamed the file

--- code/quast_PacBio_corrected_Illumina.sh
+++ code/quast_PacBio_corrected_Illumina.sh
@@ -3,7 +3,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 01:00:00
-#SBATCH -J quast_PacBio_corrected_Illumina_evaluation
+#SBATCH -J quast_PacBio_corrected_Illumina_evaluation_1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user lauri.mesilaakso.5423@student.uu.se
 
@@ -13,5 +13,6 @@ module load quast/4.5.4
 
 # Your commands
 quast.py /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_corrected_Illumina/pilon.fasta \
--o /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_corrected_Illumina_evaluation/Quast \
+/home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
+-o /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_corrected_Illumina_evaluation/Quast_1 \
 -R /home/lame5423/Genome_analysis/data/DNA/RefGenome/efaecium_ref_genome.fasta
