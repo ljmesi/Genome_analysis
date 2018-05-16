d3ae87abfbc9b84777f1ef0b9686cd688be0892c
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-16 15:09:30 +0200

    Updated the sbatch for rna-seq bwa alignment; re-organised code folder

--- code/bwa_rna_seq_reads_mapping.sh
+++ code/bwa_rna_seq_reads_mapping.sh
@@ -2,8 +2,8 @@
 #SBATCH -A g2018003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 4:00:00
-#SBATCH -J bwa
+#SBATCH -t 8:00:00
+#SBATCH -J bwa_rna_seq_reads
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user lauri.mesilaakso.5423@student.uu.se
 
@@ -11,8 +11,10 @@
 module load bioinfo-tools
 module load bwa/0.7.17
 
-bwa index /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
-bwa mem /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
-/home/lame5423/Genome_analysis/data/DNA/Illumina/WGS_forward.fq.gz \
-/home/lame5423/Genome_analysis/data/DNA/Illumina/WGS_reverse.fq.gz \
-> aln-pe.sam
+bwa index /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_corrected_Illumina/pilon.fasta
+bwa mem rna_seq_index \
+/home/lame5423/Genome_analysis/analyses/02_preprocessing/RNA-Seq_trim_paired_1.fg.gz \
+/home/lame5423/Genome_analysis/analyses/02_preprocessing/RNA-Seq_trim_paired_2.fg.gz \
+/home/lame5423/Genome_analysis/analyses/02_preprocessing/RNA-Seq_trim_unpaired_1.fg.gz \
+/home/lame5423/Genome_analysis/analyses/02_preprocessing/RNA-Seq_trim_unpaired_2.fg.gz \
+> aln-rna_seq.sam
