ca74fa722c63e253e557de1492315c00a5bd335b
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-17 08:37:25 +0200

    Added --force

--- code/prokka.sh
+++ code/prokka.sh
@@ -13,5 +13,5 @@ module load prokka/1.12-12547ca
 
 prokka \
 --outdir /pica/h1/lame5423/Genome_analysis/analyses/04_annotation/Prokka \
---addgenes --rfam \
+-- force --addgenes --rfam \
 /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_corrected_Illumina/pilon.fasta
