9312d6cbf725507b712c2e2634debec6bae00924
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-17 08:42:02 +0200

    Corrected path

--- code/prokka.sh
+++ code/prokka.sh
@@ -12,6 +12,6 @@ module load bioinfo-tools
 module load prokka/1.12-12547ca
 
 prokka \
---outdir /pica/h1/lame5423/Genome_analysis/analyses/04_annotation/Prokka \
+--outdir /home/lame5423/Genome_analysis/analyses/04_annotation/Prokka/ \
 -- force --addgenes --rfam \
 /home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_corrected_Illumina/pilon.fasta
