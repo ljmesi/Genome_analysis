82a57a2843b0a9fcae67af812e795dc151609ba0
ljmesi_ubuntu <37740329+ljmesi@users.noreply.github.com>
2018-05-17 08:32:23 +0200

    Sbatch script for prokka

---
+++ code/prokka.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 0:30:00
+#SBATCH -J prokka
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user lauri.mesilaakso.5423@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load prokka/1.12-12547ca
+
+% prokka \
+--outdir /pica/h1/lame5423/Genome_analysis/analyses/04_annotation/Prokka \
+--addgenes --rfam \
+/home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_corrected_Illumina/pilon.fasta
