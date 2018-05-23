3ded39c54ac135256e347c6780ad0630d7447a96
lame5423_rackham_genome_analysis <37740329+ljmesi@users.noreply.github.com>
2018-05-23 22:13:56 +0200

    Added mummerplot script and moved the slurm of htseq

---
+++ code/mummerplot_corrected_assembly.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 0:10:00
+#SBATCH -J mummerplot_corrected_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user lauri.mesilaakso.5423@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load MUMmer/3.9.4alpha
+
+mummer -mum /home/lame5423/Genome_analysis/data/DNA/RefGenome/efaecium_ref_genome.fasta \
+/home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_corrected_Illumina/1.fasta \
+> matches_assembly_corrected.mummer
+
+mummerplot -r /home/lame5423/Genome_analysis/data/DNA/RefGenome/efaecium_ref_genome.fasta -png "new_corrected_assembly.png" matches_assembly_corrected.mummer
