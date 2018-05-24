01071c12e279a4a217ac9e74ec6d197f36a69cd8
lame5423_rackham_genome_analysis <37740329+ljmesi@users.noreply.github.com>
2018-05-24 08:23:35 +0200

    Testing visualisation with Pacbio contigs only.

---
+++ code/mummerplot_assembly.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 0:10:00
+#SBATCH -J mummerplot_corrected_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user lauri.mesilaakso.5423@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load MUMmer/3.9.4alpha
+
+mummer -mum /home/lame5423/Genome_analysis/data/DNA/RefGenome/efaecium_ref_genome.fasta \
+/home/lame5423/Genome_analysis/analyses/03_genome_assembly/PacBio_assembly/efaecium.contigs.fasta \
+> matches_assembly.mummer
+mummerplot -t png matches_assembly.mummer
